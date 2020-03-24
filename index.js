12c4e7ec5c23214652edaf94b0edce1ac27d3b34
Alexandr Balasyan <mywebstreet@gmail.com>
2020-03-24 08:54:40 +0300

    added uncaughtException handling

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 const Node = require('./src/node')();
 const config = require('./museria.config');
 const runner = require('./runner');
+
+process.on('uncaughtException', (err) => { 
+  console.error(err.stack);
+  process.exit(1);
+});
+
 runner(config, Node);
